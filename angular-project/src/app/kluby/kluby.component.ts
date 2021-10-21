4b1ed16c7c281e839d5920c6ef88b52d98c8659e
Joachim <you@example.com>
2021-10-21 12:51:54 +0200

    Click Events

--- angular-project/src/app/kluby/kluby.component.ts
+++ angular-project/src/app/kluby/kluby.component.ts
@@ -14,6 +14,7 @@ import { Klub } from '../klub';
 export class KlubyComponent implements OnInit {
   
   kluby = KLUBY;
+  selectedKlub?: Klub;
  
   klub: Klub = {
     id: 1,
@@ -32,4 +33,7 @@ export class KlubyComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSelect(klub: Klub): void {
+     this.selectedKlub = klub;
+  }
 }
