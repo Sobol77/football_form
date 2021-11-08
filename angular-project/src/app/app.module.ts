34c1fcf0d720659e53e59ffef6471ac73586fe89
Joachim <you@example.com>
2021-11-08 12:41:51 +0100

    adding form

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -2,12 +2,16 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { KlubyComponent } from './kluby/kluby.component';
 import { KlubDetailComponent } from './klub-detail/klub-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -19,9 +23,14 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    FormsModule
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot( InMemoryDataService,  {
+      dataEncapsulation: false 
+    }),
+    FormsModule,
+    AppRoutingModule
   ],
+   
   providers: [],
   bootstrap: [AppComponent]
 })
